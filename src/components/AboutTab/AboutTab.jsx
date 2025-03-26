4c43621956513a7b9e6cc20b37cde4539e6bc0ff
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-03-26 20:47:12 +0100

    implemented about tab

--- src/components/AboutTab/AboutTab.jsx
+++ src/components/AboutTab/AboutTab.jsx
@@ -1,8 +1,19 @@
 import classes from "./AboutTab.module.css";
+import { AboutTabTextContent } from "../../text_contents/about_tab_texts";
 
 export default function () {
+    let content = [];
+    for (let i = 0; i < AboutTabTextContent.headings.length; i++) {
+        content = [...content, (<h1>{AboutTabTextContent.headings[i]}</h1>)];
+        content = [...content, (<p>{AboutTabTextContent.paragraphs[i]}</p>)];
+    }
+
     return (
-        <div className={classes.about_tab + " tab"}>
-            about
-        </div>);
+        <div className={classes.about_tab}>
+            <div className={classes.text + " tab"}>
+                {content}
+            </div>
+        </div>
+    );
+
 }
---
+++ src/text_contents/about_tab_texts.jsx
@@ -0,0 +1,10 @@
+export const AboutTabTextContent = {
+    headings: ["What is a Distraction Notebook", "Why use it", "How to use it", "Who is it for", "Does it work"],
+    paragraphs: [
+        "In a world full of endless notifications, constant noise, and scattered thoughts, staying focused feels impossible. But what if you had a place to capture distractions, clear your mind, and regain control over your attention?",
+        "Distraction Notebook is your personal tool to declutter your thoughts and sharpen your focus. Instead of letting interruptions derail your flow, jot them down and return to what truly matters. Think clearer, work smarter, and stay in control—one note at a time.",
+        "Start your journey to undistracted thinking today!",
+        "Everybody",
+        "Yes, I've tried"
+    ]
+}
