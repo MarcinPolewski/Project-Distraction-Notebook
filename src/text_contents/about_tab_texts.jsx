6ed8227106274bffa0b004f67b37f3ae26fb7144
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-03-29 10:30:13 +0100

    small changes to about tab

--- src/text_contents/about_tab_texts.jsx
+++ src/text_contents/about_tab_texts.jsx
@@ -1,9 +1,9 @@
 export const AboutTabTextContent = {
-    headings: ["What is a Distraction Notebook", "Why use it", "How to use it", "Who is it for", "Does it work"],
+    headings: ["What is a Distraction Notebook?", "How to use it?", "Why use it?", "Who is it for?", "Does it work?"],
     paragraphs: [
         "In a world full of endless notifications, constant noise, and scattered thoughts, staying focused feels impossible. But what if you had a place to capture distractions, clear your mind, and regain control over your attention?",
+        "When you’re working, keep your distraction notebook within reach. Whenever a disruptive thought comes (like ‘I forgot to call back John,’ or ‘I need to read about this topic, seems interesting’), simply write it down, continue with your focus session, and address it later! It’s as simple as that.",
         "Distraction Notebook is your personal tool to declutter your thoughts and sharpen your focus. Instead of letting interruptions derail your flow, jot them down and return to what truly matters. Think clearer, work smarter, and stay in control—one note at a time.",
-        "Start your journey to undistracted thinking today!",
         "Everybody",
         "Yes, I've tried"
     ]
